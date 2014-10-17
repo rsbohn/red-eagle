8833304426046d9126d3b3c17a4687b726287686
Randall Bohn <rsbohn@familysearch.org>
2014-10-17 05:39:28 -0600

    expand_or_contract

--- js/editor.js
+++ js/editor.js
@@ -2,6 +2,8 @@ var editor = CodeMirror(document.getElementById('editor'),
         { lineNumbers: true });
         
 editor.setValue(localStorage.scratch || "Welcome to Red Eagle.");
+editor.on('dblclick', B.expand_or_contract);
+
 var inject = function(url) {
     var script = document.createElement('script');
     script.src = url;
@@ -10,11 +12,53 @@ var inject = function(url) {
         
 var B = {};
 B.line = {
-    current: function(editor) {return editor.getValue();}
+    current: function(editor, offset) {
+        var dot = editor.getCursor();
+        offset = offset || 0;
+        return editor.getRange({line:dot.line+offset, ch:0},
+            {line:dot.line+offset});
+    },
+    indent: function(editor, offset) {
+        return B.line.current(editor, offset)
+            .match("^( *)")[1];
+    }
 };
 
 B.splice = function(editor, text) {
     editor.setCursor({line:editor.getCursor().line})
     editor.replaceSelection(text);
-    return editor;
+};
+B.kill_line = function(editor, offset) {
+    var dot = editor.getCursor();
+    var original = {line:dot.line, ch:dot.ch};
+    dot.line += offset || 0;
+    editor.replaceRange("", {line:dot.line, ch: 0}, {line:dot.line+1, ch:0});
+    editor.setCursor(original);
 }
+B.tree = {
+    has_children: function(editor){
+        var my_indent = B.line.indent(editor).length;
+        var next_indent = B.line.indent(editor,1).length;
+        return (next_indent > my_indent);
+    },
+    remove_children: function(editor){
+        while(B.tree.has_children(editor)) {
+            B.kill_line(editor, +1);
+        }
+    }
+};
+B.expand_or_contract = function(editor, event) {
+    if (B.tree.has_children(editor)) {
+        console.log("has_children=true");
+        B.tree.remove_children(editor);
+    } else {
+        var text = "\n"+B.line.indent(editor)+"  | cows";
+        B.splice(editor, text);
+    }
+    event.codemirrorIgnore=true;
+};
+
+var Menus = {
+    hello: function(){return "Welcome to Red Eagle."},
+    echo: function(args){return JSON.stringify(args)}
+};
