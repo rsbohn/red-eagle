8ec2dedf8c6a0d80bb0ffe62861d2d6b240d0149
Randall Bohn <rsbohn@familysearch.org>
2014-10-16 20:19:07 -0600

    start editor.js

---
+++ js/editor.js
@@ -0,0 +1,20 @@
+var editor = CodeMirror(document.getElementById('editor'),
+        { lineNumbers: true });
+        
+editor.setContent(localStorage.scratch || "Welcome to Red Eagle.");
+var inject = function(url) {
+    var script = document.createElement('script');
+    script.src = url;
+    document.head.appendChild(script);
+}
+        
+var B = {};
+B.line = {
+    current: function(editor) {return editor.getValue();}
+};
+
+B.splice = function(editor, text) {
+    editor.setCursor({line:editor.getCursor.line})
+    editor.replaceSelection(text);
+    return editor;
+}
